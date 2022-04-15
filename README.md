8d4a1b60a620ec0ecb880e78edb79b0ac41e4564
HeyItsJono <freymotifs@gmail.com>
2022-04-15 20:45:54 +1000

    Fix allowing multiple origins, update Readme.md

--- README.md
+++ README.md
@@ -4,10 +4,30 @@ A Python Flask-based proxy adding Access-Control-Allow-Origin to all responses.
 A fork of cors-proxy with the added feature of being deployable via docker, allowing you to define Origins via environment variables, and allowing you to set Access-Control-Allow-Credentials to true via environment variables (default remains false).
 
 ## Instructions
-1. 
-You can then access `http://127.0.0.1:5000/<URL>` for any `URL` and any HTTP method. The response content and return code will be forwarded.
+Can be run from either `docker` or `docker-compose`.
 
-Example: Access `https://jsonplaceholder.typicode.com/posts` with `http://127.0.0.1:5000/https://jsonplaceholder.typicode.com/posts`.
+For docker-compose:
+Set up your `docker-compose.yml` file to include pycors, for example:
 
+	services:
+  	  pycors:
+    	container_name: pycors
+    	image: heyitsjono/pycors:latest
+    	ports:
+      	+ 5757:5000
+    	environment:
+      	+ ALLOW_ORIGIN=http://URL1,https://URL1,http://URL2
+      	+ ALLOW_CREDENTIALS=false
+    	restart: unless-stopped
 
-Docker Usage
+Set `ALLOW_ORIGIN` to one origin (the IP/URL making the request to the pycors), or a comma-delimited list of origins.
+Alternatively, you can also set `ALLOW_ORIGIN` to `*` to allow requests from any origin.
+By default, `ALLOW ORIGIN` is not set to any value, in other words by default `Access-Control-Allow-Origin` will not be set to any value.
+
+Set `ALLOW_CREDENTIALS` to `true` to enable `Access-Control-Allow-Credentials`, by default this is disabled.
+
+From there just run `docker-compose up -d pycors`.
+
+You should then be able to make requests to`http://127.0.0.1:5757/<URL>` for any `URL` and any HTTP method. The response content and return code will be returned to your service with the above `Access-Control-Allow-Origin/-Credentials` defined in the environment variables set.
+
+For example: Access `https://jsonplaceholder.typicode.com/posts` with `http://127.0.0.1:5757/https://jsonplaceholder.typicode.com/posts`.
--- pyCors.py
+++ pyCors.py
@@ -12,9 +12,6 @@ def printf(text):
 
 app = flask.Flask(__name__)
 
-ALLOW_ORIGIN = getenv('ALLOW_ORIGIN')
-ALLOW_CREDENTIALS = getenv('ALLOW_CREDENTIALS', 'false')
-
 method_requests_mapping = {
     'GET': requests.get,
     'HEAD': requests.head,
@@ -38,17 +35,26 @@ def proxy(url):
         response = flask.Response(flask.stream_with_context(request.iter_content()),
                                   content_type=request.headers['content-type'],
                                   status=request.status_code)
-    if len(ALLOW_ORIGIN.split("'")) == 1:
+
+    ALLOW_ORIGIN = getenv('ALLOW_ORIGIN')
+    ALLOW_CREDENTIALS = getenv('ALLOW_CREDENTIALS', 'false')
+
+    if len(ALLOW_ORIGIN.split(",")) == 1:
+        printf("Only one origin defined in Allowed Origins list, setting Allowed Origin to "+str(ALLOW_ORIGIN))
         response.headers['Access-Control-Allow-Origin'] = ALLOW_ORIGIN
     else:
-        ALLOW_ORIGIN = ALLOW_ORIGIN.split("'")
+        ALLOW_ORIGIN = ALLOW_ORIGIN.replace("'","").split(",")
         if 'HTTP_ORIGIN' in flask.request.environ and flask.request.environ['HTTP_ORIGIN'] in ALLOW_ORIGIN:
+            printf("Request to this service is from an Origin defined in Allowed Origins list, setting Allow Origin to your origin.")
+            printf("Allowed Origin set to "+flask.request.environ['HTTP_ORIGIN'])
             response.headers['Access-Control-Allow-Origin'] = flask.request.environ['HTTP_ORIGIN']
         elif 'HTTP_ORIGIN' not in flask.request.environ:
-            printf("No HTTP_ORIGIN in initial request to this service, unable to cross-reference against Allowed Origins list. Setting Allow-Origin to first item in Allowed Origin list as default.")
+            printf("No HTTP_ORIGIN in initial request to this service, unable to cross-reference against Allowed Origins list. Setting Allow-Origin to first item in Allowed Origin list as default. This can occur when not making a request to this proxy service from a browser, for example if making a request from curl.")
+            printf("Allowed Origin set to: "+ALLOW_ORIGIN[0])
             response.headers['Access-Control-Allow-Origin'] = ALLOW_ORIGIN[0]
         elif 'HTTP_ORIGIN' in flask.request.environ and flask.request.environ['HTTP_ORIGIN'] not in ALLOW_ORIGIN:
-            printf("Origin of initial request to this service ("+flask.request.environ['HTTP_ORIGIN']+") does not match an Origin in the Allowed Origins list.")
+            printf("Origin of initial request to this service does not match an Origin in the Allowed Origins list.")
+            printf("Your Origin "+flask.request.environ['HTTP_ORIGIN'])
             printf("Allowed Origins:\n"+'\n'.join(ALLOW_ORIGIN))
     if ALLOW_CREDENTIALS == 'true':
         response.headers['Access-Control-Allow-Credentials'] = 'true'
