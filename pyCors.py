0863253c99af059b8c622fba1dac61a3f51ffe73
HeyItsJono <freymotifs@gmail.com>
2022-04-15 18:54:20 +1000

    Improved support for multiple Allowed Origins.

--- pyCors.py
+++ pyCors.py
@@ -4,10 +4,11 @@ import flask
 import requests
 from os import getenv 
 
-# To enable printing to the logs/debug console, uncomment the below three lines and use printf() instead of print()
-# import sys
-# def printf(text):
-#     print(text, file=sys.stderr)
+# Enables printing to the logs/debug console, use printf() instead of print()
+from sys import stderr
+
+def printf(text):
+    print(text, file=stderr)
 
 app = flask.Flask(__name__)
 
@@ -37,7 +38,18 @@ def proxy(url):
         response = flask.Response(flask.stream_with_context(request.iter_content()),
                                   content_type=request.headers['content-type'],
                                   status=request.status_code)
-    response.headers['Access-Control-Allow-Origin'] = ALLOW_ORIGIN
+    if len(ALLOW_ORIGIN.split("'")) == 1:
+        response.headers['Access-Control-Allow-Origin'] = ALLOW_ORIGIN
+    else:
+        ALLOW_ORIGIN = ALLOW_ORIGIN.split("'")
+        if 'HTTP_ORIGIN' in flask.request.environ and flask.request.environ['HTTP_ORIGIN'] in ALLOW_ORIGIN:
+            response.headers['Access-Control-Allow-Origin'] = flask.request.environ['HTTP_ORIGIN']
+        elif 'HTTP_ORIGIN' not in flask.request.environ:
+            printf("No HTTP_ORIGIN in initial request to this service, unable to cross-reference against Allowed Origins list. Setting Allow-Origin to first item in Allowed Origin list as default.")
+            response.headers['Access-Control-Allow-Origin'] = ALLOW_ORIGIN[0]
+        elif 'HTTP_ORIGIN' in flask.request.environ and flask.request.environ['HTTP_ORIGIN'] not in ALLOW_ORIGIN:
+            printf("Origin of initial request to this service ("+flask.request.environ['HTTP_ORIGIN']+") does not match an Origin in the Allowed Origins list.")
+            printf("Allowed Origins:\n"+'\n'.join(ALLOW_ORIGIN))
     if ALLOW_CREDENTIALS == 'true':
         response.headers['Access-Control-Allow-Credentials'] = 'true'
     return response
